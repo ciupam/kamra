2a441d64281085d839ab0dad3a58d0182ca8ba7d
MateuszCiupa <01131212@pw.edu.pl>
2020-03-30 14:33:31 +0200

    almost done

--- src/index.js
+++ src/index.js
@@ -1,74 +1,130 @@
+"use strict";
+
 const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
 const getHeight = () => window.innerHeight;
 const getWidth = () => window.innerWidth;
+const getWindowSize = () => [
+    getWidth(),
+    getHeight()
+];
 
 const setCanvasDimensions = () => {
     canvas.width = getWidth();
     canvas.height = getHeight();
 };
 
-window.addEventListener("resize", setCanvasDimensions);
+window.addEventListener("resize", () => {
+    setCanvasDimensions();
+    render();
+});
 setCanvasDimensions();
 
-
-
-const MODEL_MAX_X = 2, MODEL_MIN_X = -2;
-const MODEL_MAX_Y = 2, MODEL_MIN_Y = -2;
-
-const perspectiveProjection = ([x, y, z]) => [
-    x / (z+4),
-    y / (z+4)
-];
-
-const project = point => {
-    const [x, y] = perspectiveProjection(point);
-    return [
-        getWidth() * (x - MODEL_MIN_X) / (MODEL_MAX_X - MODEL_MIN_X),
-        getHeight() * (1 - (y - MODEL_MIN_Y) / (MODEL_MAX_Y - MODEL_MIN_Y))
-    ];
-}
-
-const renderPoint = point => {
-    const [x, y] = project(point);
-
+const renderLine = ([xa, ya], [xb, yb]) => {
     ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(x+1, y+1);
-    ctx.lineWidth = 4;
+    ctx.moveTo(xa, ya);
+    ctx.lineTo(xb, yb);
+    ctx.lineWidth = 2;
     ctx.strokeStyle = 'white';
     ctx.stroke();
 };
 
-let points = [];
-for (let x=-1; x<=1; x+=0.5)
-    for (let y=-1; y<=1; y+=0.5)
-        for (let z=-1; z<=1; z+=0.5)
-            points.push([x, y, z]);
+const multiply = (A, B) => {
+    const [ma, mb, na] = [A.length, B.length, A[0].length];
+    if (na !== mb) throw new Error('matrix from mult wrong sizes!');
+    const C = new Array(ma);
 
-const rotateY = ([x, y, z], theta) => [
-    Math.cos(theta) * x - Math.sin(theta) * z,
-    y,
-    Math.sin(theta) * x + Math.cos(theta) * z
-];
+    if (Array.isArray(B[0])) {
+        const nb = B[0].length;
+        for (let i=0; i<ma; i++) C[i] = new Array(nb).fill(0);
 
-const rotateX = ([x, y, z], theta) => [
-    x,
+        for (let i=0; i<ma; i++)
+            for (let j=0; j<nb; j++)
+                for (let k=0; k<na; k++) 
+                    C[i][j] += A[i][k]*B[k][j];
+    } else {
+        C.fill(0);
+        for (let i=0; i<ma; i++)
+            for (let k=0; k<na; k++)
+                C[i] += A[i][k]*B[k];
+    }   
     
+    
+    return C;
+};
+
+// normalize 
+const cube = [
+    [[-1, -1, -10, 1], [1, -1, -10, 1]],
+    [[-1, -1, -10, 1], [-1, 1, -10, 1]],
+    [[-1, -1, -10, 1], [-1, -1, -12, 1]],
+    [[1, -1, -10, 1], [1, -1, -12, 1]],
+    [[1, -1, -10, 1], [1, 1, -10, 1]],
+    [[1, -1, -12, 1], [-1, -1, -12, 1]],
+    [[1, -1, -12, 1], [1, 1, -12, 1]],
+    [[-1, 1, -10, 1], [1, 1, -10, 1]],
+    [[-1, -1, -12, 1], [-1, 1, -12, 1]],
+    [[-1, 1, -10, 1], [-1, 1, -12, 1]],
+    [[-1, 1, -12, 1], [1, 1, -12, 1]],
+    [[1, 1, -12, 1], [1, 1, -10, 1]]
 ];
 
-let theta = 0.1;
-const dtheta = 0.02;
+// orthocraphic view volume based on camera coord system
+const orthViewVolume = {
+    l: -10, // left
+    r: 10, // right
+    b: -10, // bottom
+    t: 10, // top
+    n: -19, // near,
+    f: -20 // far
+};
+
+// orthographic to canonical view volume (matrix)
+const morth = ({ l, r, b, t, n, f }) => [
+    [2/(r-l), 0, 0, (r+l)/(l-r)],
+    [0, 2/(t-b), 0, (t+b)/(b-t)],
+    [0, 0, 2/(n-f), (n+f)/(f-n)],
+    [0, 0, 0, 1]
+];
+
+// viewport transformation matrix
+const mvp = () => {
+    const [nx, ny] = getWindowSize();
+    return [
+        [nx/2, 0, 0, (nx-1)/2],
+        [0, ny/2, 0, (ny-1)/2],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+    ];
+};
+
+// perspective to cannonical view volume (matrix)
+const mper = ({ l, r, b, t, n, f }) => [
+    [2*n/(r-l), 0, (l+r)/(l-r), 0],
+    [0, 2*n/(t-b), (b+t)/(b-t), 0],
+    [0, 0, (f+n)/(n-f), 2*f*n/(f-n)],
+    [0, 0, 1, 0]
+];
+
+const Morth = morth(orthViewVolume);
+const Mper = mper(orthViewVolume);
 
 const render = () => {
-    ctx.clearRect(0, 0, getWidth(), getHeight());
-    theta += dtheta;
-    points.forEach(point => {
-        point = rotateY(point, theta);
-        renderPoint(point);
-    });
-    requestAnimationFrame(render);
+//     ctx.clearRect(0, 0, getWidth(), getHeight());
+//     theta += dtheta;
+//     points.forEach(point => {
+//         point = rotateY(point, theta);
+//         renderPoint(point);
+//     });
+//     requestAnimationFrame(render);
+    const Mvp = mvp();
+    const M = multiply(Mvp, Mper);
+    cube.forEach(v => {
+        const [xp, yp, zp, wp] = multiply(M, v[0]);
+        const [xq, yq, zq, wq] = multiply(M, v[1]);
+        renderLine([xp/wp, yp/wp], [xq/wq, yq/wq]);
+    })
 };
 
 render();
